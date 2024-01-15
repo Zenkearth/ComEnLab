7bb3a03c88fa03a7c93aecee01c12dff8e4743f4
Zenkearth <peeraphatnaowasaisee1@gmail.com>
2024-01-15 07:35:16 +0000

    First commit

---
+++ lab6_1.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+LED = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED,GPIO.OUT)
+while True:
+    GPIO.output (LED,True)
+    time.sleep(0.1)
+    GPIO.output (LED,False)
+    time.sleep(1)
